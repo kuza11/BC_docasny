1920a376182a08f06f998d17ce8d6e0af8bab2bb
Jakub Zvolánek <ZvolanekJ@ddmp9.local>
2020-01-09 19:25:49 +0100

    zprovozneni

--- test.php
+++ test.php
@@ -2,13 +2,15 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
 	$file = fopen("test.json", "w");
-	fwrite($file, $_POST["data"]);
+	fwrite($file, file_get_contents("php://input"));
+	echo file_get_contents("php://input");
 	fclose($file);
 } else 
 {
 	$file = fopen("test.json", "r");
 	echo fgets($file);
 	fclose($file);
+
 }
  ?>
  
